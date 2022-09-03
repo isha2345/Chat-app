5bf032bf27baa8fa598a2aeaee60ee26ab9c25cb
Isha <ishabhanot3@gmail.com>
2022-09-03 23:28:05 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+const config={
+    apiKey: "AIzaSyD7Aw9oEnh_153Bcox170fFDjLWvUJ1Qyk",
+    authDomain: "chat-web-app-765ef.firebaseapp.com",
+    databaseURL: "https://chat-web-app-765ef-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-765ef",
+    storageBucket: "chat-web-app-765ef.appspot.com",
+    messagingSenderId: "997696899996",
+    appId: "1:997696899996:web:790ac3dd50fba4e0bed659"
+};
+const app = firebase.initializeApp(config);  
